e74270311263d9127e33e4895e508fcd1529d438
philipp <philipp@email.com>
2025-10-16 23:20:33 +0200

    Connection test of all hardware

---
+++ include/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Pin definitions
+#define PIN_UART_RX    2 // Software serial for audio module
+#define PIN_UART_TX    3 // Software serial for audio module
+#define PIN_WALL_OPEN  4 // Relais, to internal transponder
+#define PIN_WALL_CLOSE 5 // Relais, to internal transponder
+#define PIN_230V       6 // Relais, to socket
+#define PIN_FOG        7 // Relais, to JST-SM X2
+#define PIN_MOTOR_STATE_GREEN 8 // JST-SM X3 connector
+#define PIN_MOTOR_STATE_RED   9 // JST-SM X3 connector
+#define PIN_TRIG      10 // JST-SM X2 connector
+#define PIN_SWITCH_1  15 // UI on controller box
+#define PIN_SWITCH_2  16 // UI on controller box
+#define PIN_BUTTON    17 // UI on controller box
+#define PIN_LED_RED   18 // UI on controller box
+#define PIN_LED_GREEN 19 // UI on controller box
+
+#endif
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,88 @@
 #include <Arduino.h>
+#include <config.h>
+#include <MD_YX5300.h>
+#include <SoftwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+SoftwareSerial mp3Serial(PIN_UART_RX, PIN_UART_TX);
+MD_YX5300 mp3(mp3Serial);
+
+void configureMP3(){
+    mp3Serial.begin(MD_YX5300::SERIAL_BPS);
+    Serial.begin(115200);
+    mp3.begin();
+    mp3.setSynchronous(true);
+    mp3.playFolderRepeat(1);
+    mp3.volume(mp3.volumeMax());
+
+}
+
+void audioPlay(){
+    mp3.playStart();
+}
+
+void audioStop(){
+    mp3.playStop();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // Relay outputs
+    pinMode(PIN_WALL_OPEN, OUTPUT);
+    pinMode(PIN_WALL_CLOSE, OUTPUT);
+    pinMode(PIN_230V, OUTPUT);
+    pinMode(PIN_FOG, OUTPUT);
+    digitalWrite(PIN_WALL_OPEN, HIGH);
+    digitalWrite(PIN_WALL_CLOSE, HIGH);
+    digitalWrite(PIN_230V, HIGH);
+    digitalWrite(PIN_FOG, HIGH);
+
+    // Indicator LEDs
+    pinMode(PIN_LED_RED, OUTPUT);
+    pinMode(PIN_LED_GREEN, OUTPUT);
+
+    // Inputs
+    pinMode(PIN_MOTOR_STATE_GREEN, INPUT);
+    pinMode(PIN_MOTOR_STATE_RED, INPUT);
+    pinMode(PIN_TRIG, INPUT_PULLUP);
+    pinMode(PIN_SWITCH_1, INPUT_PULLUP);
+    pinMode(PIN_SWITCH_2, INPUT_PULLUP);
+    pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+    configureMP3();
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    mp3.check();
+    audioPlay();
+    delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    digitalWrite(PIN_WALL_OPEN, LOW);
+    delay(100);
+    digitalWrite(PIN_WALL_CLOSE, LOW);
+    delay(100);
+    digitalWrite(PIN_230V, LOW);
+    delay(100);
+    digitalWrite(PIN_FOG, LOW);
+    delay(100);
+    digitalWrite(PIN_WALL_OPEN, HIGH);
+    delay(100);
+    digitalWrite(PIN_WALL_CLOSE, HIGH);
+    delay(100);
+    digitalWrite(PIN_230V, HIGH);
+    delay(100);
+    digitalWrite(PIN_FOG, HIGH);
+
+    if(!digitalRead(PIN_SWITCH_1)) digitalWrite(PIN_LED_GREEN, HIGH);
+    else digitalWrite(PIN_LED_GREEN, LOW);
+
+    if(!digitalRead(PIN_SWITCH_2)) digitalWrite(PIN_LED_RED, HIGH);
+    else digitalWrite(PIN_LED_RED, LOW);
+
+    if(!digitalRead(PIN_BUTTON)) Serial.println("Button pushed");
+
+    if(!digitalRead(PIN_TRIG)) Serial.println("Trigger");
+
+    if(digitalRead(PIN_MOTOR_STATE_GREEN)) Serial.println("Motor green");
+    if(digitalRead(PIN_MOTOR_STATE_RED)) Serial.println("Motor red");
+
 }
